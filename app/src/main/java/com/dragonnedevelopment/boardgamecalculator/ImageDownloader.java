b8370e8cbf6f881b20ef200b311a1a1b5969825c
Muir Halleron <muir.halleron@gmail.com>
2017-10-23 09:20:03 +0100

    Rearrange the menu so that the titles are alphabetical (numbers first, Single Point game button last)

--- app/src/main/java/com/dragonnedevelopment/boardgamecalculator/ImageDownloader.java
+++ app/src/main/java/com/dragonnedevelopment/boardgamecalculator/ImageDownloader.java
@@ -16,7 +16,11 @@ public class ImageDownloader {
     // Create an ArrayList of games.
     final static ArrayList<Game> games = new ArrayList<>();
 
-    static void downloadImage(Context context) {
+    interface Callback {
+        void onDone(ArrayList<Game> games);
+    }
+
+    static void downloadImage(Context context, final Callback callback) {
 
         // image resources
         int alhambraResourceId = R.drawable.alhambra;
@@ -28,16 +32,19 @@ public class ImageDownloader {
         int takenokoResourceId = R.drawable.takenoko;
         int plusMinusResourceId = R.drawable.plusminus;
 
+
         // Fill ArrayList with Game objects
+        games.add(new Game(context.getString(R.string.sevenwonders_game_name), sevenWondersResourceId));
         games.add(new Game(context.getString(R.string.alhambra_game_name), alhambraResourceId));
         games.add(new Game(context.getString(R.string.ascension_game_name), ascensionResourceId));
-        games.add(new Game(context.getString(R.string.catan_game_name), catanResourceId));
         games.add(new Game(context.getString(R.string.caverna_game_name), cavernaResourceId));
         games.add(new Game(context.getString(R.string.eclipse_game_name), eclipseResourceId));
-        games.add(new Game(context.getString(R.string.sevenwonders_game_name), sevenWondersResourceId));
+        games.add(new Game(context.getString(R.string.catan_game_name), catanResourceId));
         games.add(new Game(context.getString(R.string.takenoko_game_name), takenokoResourceId));
         games.add(new Game(context.getString(R.string.singlepoint_game_name), plusMinusResourceId));
+        
+        callback.onDone(games);
 
     }
-
 }
+
--- app/src/main/java/com/dragonnedevelopment/boardgamecalculator/Menu.java
+++ app/src/main/java/com/dragonnedevelopment/boardgamecalculator/Menu.java
@@ -1,13 +1,64 @@
 package com.dragonnedevelopment.boardgamecalculator;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.GridView;
 
-public class Menu extends AppCompatActivity {
+import com.dragonnedevelopment.boardgamecalculator.model.Game;
+
+import java.util.ArrayList;
+
+public class Menu extends AppCompatActivity implements ImageDownloader.Callback{
+
+    Intent gameIntent;
+
+    public final static String EXTRA_GAME_NAME =
+            "com.dragonnedevelopment.boardgamecalculator.EXTRA_GAME_NAME";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        Toolbar menuToolbar = (Toolbar) findViewById(R.id.menu_toolbar);
+        setSupportActionBar(menuToolbar);
+        getSupportActionBar().setTitle(getString(R.string.menu_title));
+    }
+
+    /**
+     * Call ImageDownloader.downloadImage from onStart or onResume instead of onCreate to ensure
+     * that the images aren't downloaded too early.
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        ImageDownloader.downloadImage(this, Menu.this);
+    }
+
+    @Override
+    public void onDone(ArrayList<Game> games) {
+        /*
+         * Create a {@link GameAdapter}, whose data source is a list of {@link Game}s. The adapter
+         * knows how to create grid items for each item in the list.
+         */
+        GridView gridView = (GridView)findViewById(R.id.game_grid_view);
+        GameMenuAdapter adapter = new GameMenuAdapter(this, R.layout.grid_item_layout, games);
+        gridView.setAdapter(adapter);
+
+        /*// Set a click listener on that View
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Game item = (Game) adapterView.getItemAtPosition(position);
+
+                // set the intent to open the gameScoringActivity
+                gameIntent = new Intent(Menu.this, *//* Activity class name *//*);
+                String gameName = item.getGameName();
+                gameIntent.putExtra(EXTRA_GAME_NAME, gameName);
+                startActivity(gameIntent);
+            }
+        });*/
     }
 }
